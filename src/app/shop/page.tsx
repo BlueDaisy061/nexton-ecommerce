06b15d19b9b6c6726d45a59ca0802d343db4b2bf
Daisy Nguyen <nttduong.c3tqcap.a1@gmail.com>
2024-08-18 23:40:40 +0700

    feat: add filter categories and price range

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,3 +1,17 @@
+import { FilterCategories } from '../ui/filter-categories';
+import { FilterPriceRange } from '../ui/filter-price-range';
+
 export default function ShopPage() {
-  return <div>ShopPage</div>;
+  return (
+    <div className="pt-[4.5rem]">
+      <div className="lg:flex lg:mx-[4.5rem] lg:mt-10 lg:mb-20">
+        <div className="lg:min-w-64">
+          <FilterCategories />
+          <hr className="bg-body-text opacity-15 my-10" />
+          <FilterPriceRange />
+          <hr className="bg-body-text opacity-15 my-10" />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/app/ui/filter-price-range.tsx
@@ -0,0 +1,68 @@
+'use client';
+import { InputNumber, Slider } from 'antd';
+import { useState } from 'react';
+
+type RangePriceType = {
+  min: number;
+  max: number;
+};
+
+export const FilterPriceRange = () => {
+  const [range, setRange] = useState<RangePriceType>({ min: 0, max: 500 });
+
+  const onChange = (value: number[]) => {
+    if (value[0] < value[1]) {
+      setRange({ min: value[0], max: value[1] });
+    }
+  };
+
+  const onChangeMin = (value: any) => {
+    if (range.max && range.max > value) {
+      setRange({ min: value, max: range.max });
+    }
+  };
+
+  const onChangeMax = (value: any) => {
+    if (range.min && range.min < value) {
+      setRange({ max: value, min: range.min });
+    }
+  };
+
+  return (
+    <div>
+      <h4 className="mb-6">Price Range</h4>
+      <div>
+        <Slider
+          min={0}
+          max={500}
+          onChange={onChange}
+          range={true}
+          defaultValue={[range.min, range.max]}
+          value={[range.min, range.max]}
+        />
+        <div className="flex justify-between gap-6 mt-5">
+          <div>
+            <p>Min price</p>
+            <InputNumber
+              className="w-28 rounded-full"
+              min={0}
+              max={500}
+              value={range.min}
+              onChange={onChangeMin}
+            />
+          </div>
+          <div>
+            <p>Max price</p>
+            <InputNumber
+              className="w-28 rounded-full"
+              min={0}
+              max={500}
+              value={range.max}
+              onChange={onChangeMax}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
