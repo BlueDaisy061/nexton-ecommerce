fc6cc4a59d1c82501b7d30179a394d96bf81a0e1
Daisy Nguyen <nttduong.c3tqcap.a1@gmail.com>
2024-11-06 23:24:08 +0700

    feat: add signup endpoint

--- server/index.js
+++ server/index.js
@@ -81,7 +81,7 @@ const Users = mongoose.model('Users', {
   },
 });
 
-// App endpoints
+// App endpoints for products
 
 app.get('/', (req, res) => {
   res.send('App is running');
@@ -140,6 +140,37 @@ app.get('/all-products', async (req, res) => {
   let products = await Product.find({});
   console.log('All product fetched');
   res.send(products);
+});
+
+// App endpoints for users
+
+app.post('/signup', async (req, res) => {
+  let check = await Users.findOne({ email: req.body.email });
+  if (check) {
+    return res.status(400).json({ success: false, errors: 'Existing user found with same email.' });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+
+  await user.save();
+
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+
+  const token = jwt.sign(data, 'secret_ecom');
+  res.json({ success: true, token });
 });
 
 app.listen(port, (error) => {
